0e7bbc46a00f76bc1757e6a6c8da25c2cffdb170
ZimmermannnnamremmiZ <Maximvo92@gmail.com>
2021-03-23 00:40:07 +0300

    start

---
+++ src/plugins/axios/index.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const instance = axios.create({ // здесь находятся некие настройки для axios
+  baseURL: process.env.VUE_APP_API_URL,
+  params: {      // query параметры, которые должны передаваться с каждым запросом
+    apikey: process.env.VUE_APP_API_KEY,
+    plot: 'full'  // для получения полнной информации о фильме
+  }
+});
+
+export default instance;
---
+++ src/store/modules/movies.js
@@ -0,0 +1,11 @@
+
+
+const moviesStore = {
+  namespaced: true,
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+};
+
+export default moviesStore;
